2db3176210ac6be2c4ea862c2860562ffa7ad004
helder-hanka <hfernandes238@gmail.com>
2024-07-04 07:21:06 +0200

    Create svg func, header scss and update header func

--- src/components/pages/Header/index.tsx
+++ src/components/pages/Header/index.tsx
@@ -1,11 +1,18 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "../../../utils/styles/Sass/Header.scss";
+import KasaText from "../../Svg/KasaText";
 
 const Header = () => {
   return (
     <header>
       <nav>
         <ul>
+          <li>
+            <Link to={"/"}>
+              <KasaText />
+            </Link>
+          </li>
           <li>
             <Link to={"/"}>Accueil</Link>
           </li>
