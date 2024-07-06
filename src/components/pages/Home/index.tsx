63a3fe059ffdcc606309b343c3175d3c89d47d30
helder-hanka <hfernandes238@gmail.com>
2024-07-07 01:32:14 +0200

    Import Gallery component

--- src/components/pages/Home/index.tsx
+++ src/components/pages/Home/index.tsx
@@ -1,6 +1,7 @@
 import React, { Fragment } from "react";
 import images from "../../../utils/images/la_mere_la_montagne.jpeg";
 import ModalImg from "../../ModalImg";
+import Gallery from "../../Gallery";
 
 const Home = () => {
   return (
@@ -10,6 +11,7 @@ const Home = () => {
         alt="la mere et la montagne"
         text="Chez vous, partout et ailleurs"
       />
+      <Gallery />
     </>
   );
 };
